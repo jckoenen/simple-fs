8f5faae9c26d1432e31ee54542005c150f16ffcc
Johannes Koenen <johannes.c.koenen@gmail.com>
2023-05-09 14:15:10 +0200

    Add another case to ChannelIntegrationTest

--- src/test/kotlin/de/joekoe/simplefs/internal/ChannelIntegrationTest.kt
+++ src/test/kotlin/de/joekoe/simplefs/internal/ChannelIntegrationTest.kt
@@ -4,11 +4,15 @@ import de.joekoe.simplefs.withTempFile
 import org.junit.jupiter.api.Test
 import java.nio.ByteBuffer
 import java.nio.channels.Channels
+import java.nio.channels.FileChannel
+import kotlin.io.path.Path
+import kotlin.io.path.exists
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class ChannelIntegrationTest {
     @Test
-    fun `read channel should return what write channel wrote`() = withTempFile { raf ->
+    fun `read channel should return string that write channel wrote`() = withTempFile { raf ->
         var written = -1L
         val text = "Hello, World!"
         SimpleFsWritableChannel(raf.channel, 0) { written = it }
@@ -19,5 +23,26 @@ class ChannelIntegrationTest {
             .use { it.readText() }
 
         assertEquals(text, actual)
+    }
+
+    @Test
+    fun `read channel should return content that write channel wrote`() = withTempFile { raf ->
+        var written = -1L
+        val thisFile = Path("src/test/kotlin/de/joekoe/simplefs/internal/ChannelIntegrationTest.kt")
+        assertTrue(thisFile.exists())
+
+        FileChannel.open(thisFile)
+            .use { fc ->
+                SimpleFsWritableChannel(raf.channel, 0) { written = it }
+                    .use { wc -> fc.transferTo(0, fc.size(), wc) }
+                val rc = SimpleFsReadableChannel(raf.channel, 0, written) {}
+
+                val actual = Channels.newReader(rc, Charsets.UTF_8)
+                    .use { it.readText() }
+                val expect = Channels.newReader(fc, Charsets.UTF_8)
+                    .readText()
+
+                assertEquals(expect, actual)
+            }
     }
 }
